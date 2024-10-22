62e76330907ac44600b29ce6ba3a39e6c66fd9a6
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-10-22 13:40:36 +0600

    new: add DbConnectionThreadSafe

---
+++ Creational/Singleton/DbConnectionThreadSafe.cs
@@ -0,0 +1,26 @@
+﻿namespace Learn_DesignPatterns.Creational.Singleton
+{
+    internal class DbConnectionThreadSafe
+    {
+        private static readonly Lazy<DbConnectionThreadSafe> _instance = null!;
+        private static string _connectionString = string.Empty;
+
+        static DbConnectionThreadSafe()
+        {
+            _instance = new Lazy<DbConnectionThreadSafe>(
+                () => new DbConnectionThreadSafe(_connectionString),
+                LazyThreadSafetyMode.ExecutionAndPublication
+            );
+        }
+
+        private DbConnectionThreadSafe(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public static DbConnectionThreadSafe GetInstance(string connectionString)
+        {
+            return _instance.Value;
+        }
+    }
+}
