bcf532e6e5f1133beb64811d9221f86ee19b934b
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-10-22 11:09:53 +0600

    new: add singleton

---
+++ Creational/DbConnection.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Learn_DesignPatterns.Creational
+{
+    internal class DbConnection
+    {
+        private static DbConnection _instance = null!;
+        private string _connectionString = string.Empty;
+
+        private DbConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public static DbConnection GetInstance(string connectionString)
+        {
+            if (_instance == null)
+            {
+                _instance = new DbConnection(connectionString);
+            }
+            return _instance;
+        }
+    }
+}
