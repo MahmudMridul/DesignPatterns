36bc3bbc7b6038d220c26a3b325f81b813b79d47
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-07 10:17:55 +0600

    chore: update folder structure

---
+++ NonPatternExamples/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,70 @@
+﻿namespace Learn_DesignPatterns.NonPatternExamples.FactoryMethod
+{
+    public class Client
+    {
+        public static void ClientMain()
+        {
+            DocumentCreator creator = new DocumentCreator();
+            Document pdf = creator.CreateDocument("pdf");
+            Document word = creator.CreateDocument("word");
+        }
+    }
+
+    public class DocumentCreator
+    {
+        public Document CreateDocument(string type)
+        {
+            Document doc = null!;
+            if (type == "pdf")
+            {
+                doc = new Pdf();
+            }
+            else if (type == "word")
+            {
+                doc = new Word();
+            }
+            return doc;
+        }
+    }
+
+    public interface Document
+    {
+        void Open();
+        void Close();
+        void Save();
+    }
+
+    public class Pdf : Document
+    {
+        public void Open()
+        {
+            //logic
+        }
+
+        public void Close()
+        {
+            //logic
+        }
+        public void Save()
+        {
+            //logic
+        }
+    }
+
+    public class Word : Document
+    {
+        public void Open()
+        {
+            //logic
+        }
+
+        public void Close()
+        {
+            //logic
+        }
+        public void Save()
+        {
+            //logic
+        }
+    }
+}
