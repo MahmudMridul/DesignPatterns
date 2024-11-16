fb979db8a05a150449b56a25abda88adb6aff003
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-16 13:51:10 +0600

    new: add Adapter pattern

---
+++ Structural/Adapter/Adapter.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Learn_DesignPatterns.Structural.Adapter
+{
+    public interface IJsonAdapter
+    {
+        string GetJsonData();
+    }
+
+    public class LegacyXmlSystem
+    {
+        public string GetXmlData()
+        {
+            return "<data> some XML data </data>";
+        }
+    }
+
+    public class  XmlToJsonAdapter : IJsonAdapter
+    {
+        private LegacyXmlSystem _xmlSystem;
+
+        public XmlToJsonAdapter(LegacyXmlSystem xmlSystem)
+        {
+            _xmlSystem = xmlSystem;
+        }
+
+        public string GetJsonData()
+        {
+            string xmlData = _xmlSystem.GetXmlData();
+            // converting xml to json
+            return "{ data: some XML data converted to JSON }";
+        }
+    }
+}
---
+++ Structural/Adapter/AdapterClient.cs
@@ -0,0 +1,13 @@
+﻿namespace Learn_DesignPatterns.Structural.Adapter
+{
+    public class AdapterClient
+    {
+        public void Run()
+        {
+            LegacyXmlSystem xmlSystem = new LegacyXmlSystem();
+            IJsonAdapter adapter = new XmlToJsonAdapter(xmlSystem);
+            string jsonData = adapter.GetJsonData();
+            Console.WriteLine(jsonData);
+        }
+    }
+}
